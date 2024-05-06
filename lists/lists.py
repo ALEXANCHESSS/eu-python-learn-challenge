39f5fcc3bb59ca31de26777ff50244d52bc8918b
Aleksandr Kuznetsov <aleksandr.kuznetsov@dualbootpartners.com>
2024-05-06 10:13:39 +0200

    Solution for lists

--- lists/lists.py
+++ lists/lists.py
@@ -8,7 +8,20 @@ class ListExercise:
         :param input_list: Исходный список
         :return: Список с замененными элементами
         """
-        pass
+        max: int = 0
+        result_list: list[int] = []
+        for i in input_list:
+            if i > max:
+                max = i
+        if not max:
+            return input_list
+        else:
+            for i in input_list:
+                if i >= 0:
+                    result_list.append(max)
+                else:
+                    result_list.append(i)
+            return result_list
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
@@ -20,4 +33,22 @@ class ListExercise:
         :param query: Искомый элемент
         :return: Номер элемента
         """
-        pass
+        low: int = 0
+        mid: int = len(input_list) // 2
+        high: int = len(input_list) - 1
+
+        def start_search(low: int, mid: int, high: int) -> int:
+            if low > high:
+                return -1
+            elif input_list[mid] == query:
+                return mid
+            elif input_list[mid] < query:
+                low = mid + 1
+                mid = (low + high) // 2
+                return start_search(low, mid, high)
+            else:
+                high = mid - 1
+                mid = (low + high) // 2
+                return start_search(low, mid, high)
+
+        return start_search(low, mid, high)
